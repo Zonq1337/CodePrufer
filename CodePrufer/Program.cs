ef98e10079e7ceab2a956a85277c2df0e7d06b58
ГорбачИО <@105-03>
2025-05-19 17:06:58 +0300

    Добавьте файлы проекта.

---
+++ CodePrufer/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Diagnostics.Tracing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace CodePrufer
+{
+
+    internal class Program
+    {
+        static void Main()
+        {
+            var graph = new Dictionary<int, List<int>>();  // вершина и смежные 
+            Console.WriteLine("Введите рёбра графа");
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "bb") break;
+
+                var nodes = input.Split().Select(int.Parse).ToArray();
+                int a = nodes[0], b = nodes[1];
+
+                if (!graph.ContainsKey(a)) graph[a] = new List<int>();
+                if (!graph.ContainsKey(b)) graph[b] = new List<int>();
+
+                graph[a].Add(b);
+                graph[b].Add(a);
+            }
+            // код прюфера, степени, листья
+            var pruferCode = new List<int>();
+            var degree = new Dictionary<int, int>();
+            var leaves = new SortedSet<int>();
+
+            foreach (var node in graph.Keys)
+            {
+                degree[node] = graph[node].Count; // ^ = число соседей
+                if (degree[node] == 1)
+                    leaves.Add(node);
+            }
+
+            while (graph.Count > 2)
+            {
+                int leaf = leaves.Min;
+                leaves.Remove(leaf);
+
+                int neighbor = graph[leaf].First();
+                pruferCode.Add(neighbor);
+
+                degree[neighbor]--;
+                if (degree[neighbor] == 1)
+                    leaves.Add(neighbor);
+
+                graph[neighbor].Remove(leaf);
+                graph.Remove(leaf);
+            }
+
+            Console.WriteLine("Код Прюфера: " + string.Join(" ", pruferCode));
+        }
+    }
+}
